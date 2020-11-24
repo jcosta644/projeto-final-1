55e976bcadb0869f53cac81e240a48893a3d1e94
thalyta <thalycomp@gmail.com>
2020-11-24 13:56:04 -0300

    adicionando cors

--- src/server.js
+++ src/server.js
@@ -2,15 +2,15 @@ import express from 'express';
 import globalRoutes from './routes/globalRoutes';
 import connectDB from './config/db';
 import path from 'path';
-
+import cors from 'cors';
 
 import 'dotenv/config';
-
 
 connectDB();
 
 const server = express();
 server.use(express.json());
+server.use(cors());
 
 server.use(globalRoutes);
 server.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads') ))
